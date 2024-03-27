b280bb41358b33ad97042415be0ff422b4bd7a5c
manojparida <manojparida4@gmail.com>
2024-03-27 09:50:27 +0530

    3/27

---
+++ src/leetcode/graph/SnakesAndLadders.java
@@ -0,0 +1,22 @@
+package leetcode.graph;
+
+public class SnakesAndLadders {
+    public int snakesAndLadders(int[][] board) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 35, -1, -1, 13, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 15, -1, -1, -1, -1}
+        };
+
+        SnakesAndLadders game = new SnakesAndLadders();
+        int result = game.snakesAndLadders(board);
+        System.out.println(result);
+    }
+}
